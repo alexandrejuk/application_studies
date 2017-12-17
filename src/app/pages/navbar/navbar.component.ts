c800b8c481f793bfbbcab3f540a0d55be5d2eb3d
alexandrejuk <ale_santos.soares@hotmail.com>
2017-12-17 11:12:56 -0200

    dynamic menu to project

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { MENU } from './../../utils/menu-items';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public menuItems = [];
+
+  constructor() {
+    this.menuItems = MENU;
+   }
 
   ngOnInit() {
   }
